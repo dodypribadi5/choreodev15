a43a45ae8c9e4157867387d97fdcbabb91431099
dodypribadi5 <dodypribadi5@gmail.com>
2024-11-22 16:04:34 +0700

    Create token.js

---
+++ token.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function() {
+    $('#getcs').click(function() {
+    $("#process1").show();
+ $('.load').fadeIn();
+    setTimeout(function(){      
+location.href='https://Kelupean123.wasap.my';
+ $("#process1").hide();
+ $('.load').fadeOut();        
+         }, 2000);
+     });    
+  });   
